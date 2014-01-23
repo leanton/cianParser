d1d5a3540db8eb235df454bc5b03ac266fd2039a
Anton Le <le@physics.msu.ru>
2014-01-23 13:01:19 +0400

    Parser minor improvements and debugging

--- parser.py
+++ parser.py
@@ -9,6 +9,14 @@ import time
 from socket import timeout
 from bs4 import BeautifulSoup
 
+'''Global variables of the links'''
+HREF1 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room1=1&p={}'
+HREF2 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room2=1&p={}'
+HREF3 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room3=1&p={}'
+HREF4 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room4=1&p={}'
+HREF5 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room4=1&p={}'
+HREF6 = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room4=1&p={}'
+
 
 def loadHelper(uri):
 	opener = urllib2.build_opener()
@@ -16,20 +24,20 @@ def loadHelper(uri):
 	try:
 		thing = opener.open(uri, None, 10)
 		soup = BeautifulSoup(thing.read(), "lxml")
+		return soup
 	except (timeout, urllib2.HTTPError, urllib2.URLError) as error:
 		sys.stdout.write("{} encountered, hold on, bro".format(error))
 		sys.stdout.flush()
 		time.sleep(30)
 		loadHelper(uri)
-	return soup
 
 def parsePage(pageNum):
-	uri = 'http://www.cian.ru/cat.php?deal_type=1&obl_id=1&type=4&room1=1&p={}'.format(pageNum)
+	uri = HREF1.format(pageNum)
 	soup = loadHelper(uri)
 	#print(soup.prettify())
-	table = soup.find_all('tr', id=re.compile('tr_[0-9]'))
-	sampleRow = table[0]
-
+	table = soup.find_all('tr', id=re.compile('tr_(\d+)'))
+	if (table == []):
+		print "no table"
 	data_table = []
 
 	for sampleRow in table:
@@ -37,40 +45,195 @@ def parsePage(pageNum):
 		data_row = []
 		for field in fields:	
 			data = sampleRow.find_all('td', id=re.compile('(.)_' + field))[0]
+
+			''' Parsing code is written with if statement, need to refactor, seems to be too stupid '''
+
+			''' Parsing address <td>'''
 			if (field == fields[0]):
-				adress = data.get_text('|', strip = True).split('|')
-				if len(adress) == 5:
-					data_row.append(adress[0] + ", " + adress[1] + ", " + adress[2])
-				elif len(adress) == 4:
-					data_row.append(adress[0] + ", " + adress[1])
-				elif len(adress) == 3:
-					data_row.append(adress[0])
-				elif len(adress) == 6:
-					data_row.append(adress[0] + ", " + adress[1] + ", " + adress[2]  + ", " + adress[3])
-				elif len(adress) == 7:
-					data_row.append(adress[0] + ", " + adress[1] + ", " + adress[2]  + ", " + adress[3] + ", " + adress[4])
-				data_row.append(adress[-1])
-				data_row.append(adress[-2])
-				#print adress.split('|')
-				#print repr(adress).decode("unicode-escape")
-		print repr(data_row).decode("unicode-escape")
+				address = data.get_text('|', strip = True).split('|')
+				line = ""
+				for i in range(len(address)-2):
+					if (i != len(address)-2):
+						line += address[i] + ", "
+					else:
+						line += address[i]
+				data_row.append(line)
+				data_row.append(address[-1])
+				data_row.append(address[-2])
+				#print address.split('|')
+				#print repr(address).decode("unicode-escape")
+
+			if (field == fields[1]):
+				room = data.get_text()
+				#print repr(room).decode("unicode-escape")
+				data_row.append(room)
+
+			'''Room space'''
+			if (field == fields[2]):
+				rooms = data.get_text('|', strip = True)
+
+				'''Kitchen space'''
+				kitchen = re.search(u'кухня:(\W)\d+', rooms)
+				if (kitchen != None):
+					data_row.append(kitchen.group())
+				else:
+					data_row.append(u'NULL')
+
+				'''Dwelling space'''
+				dspace = re.search(u'жилая:(\W)\d+', rooms)
+				if (dspace != None):
+					data_row.append(dspace.group())
+				else:
+					data_row.append(u'NULL')
+
+				'''Full space'''
+				space = re.search(u'общая:(\W)\d+', rooms)
+				if (space != None):
+					data_row.append(space.group())
+				else:
+					data_row.append(u'NULL')
+				
+			'''Price of the appartments'''
+			if (field == fields[3]):
+				price_raw = data.get_text('|', strip = True).split('|')
+				price = price_raw[0]
+				if (price != None):
+					data_row.append(price)
+				else:
+					data_row.append(u'NULL')
+
+			'''# of the floor'''
+			if (field == fields[4]):
+				floor = data.get_text('|', strip = True)
+				data_row.append(floor)
+
+			'''Additional information'''
+			if (field == fields[5]):
+				addinfo = data.get_text('|', strip = True)
+				data_row.append(addinfo)
+
+		
+		'''End of data to parse, appending row to a table'''		
+		#print repr(data_row).decode("unicode-escape")
 		data_table.append(data_row)
-	print repr(data_table).decode("unicode-escape")
+	#print repr(data_table).decode("unicode-escape")
 
 	# returns an array of arrays with data cells
 	return data_table
 
 
-def export(data):
-	f = codecs.open('foo.txt', 'w+', 'utf-8')
+def export(data, filename = "foo.txt"):
+	f = codecs.open(filename, 'w+', 'utf-8')
 	for row in data:
 		for cell in row:
 			f.write(cell)
 			f.write(u'	')
 		f.write(u'\n')
-	
+
+def getTable(pageNum, URL = HREF1):
+	uri = URL.format(pageNum)
+	soup = loadHelper(uri)
+	#print(soup.prettify())
+	table = soup.find_all('tr', id=re.compile('tr_(\d+)'))
+	return table
+
+def parseTable(table):
+	data_table = []
+
+	for sampleRow in table:
+		fields = [u'metro', u'room', u'rooms', u'price', u'floor', u'dopsved']
+		data_row = []
+		for field in fields:	
+			data = sampleRow.find_all('td', id=re.compile('(.)_' + field))[0]
+
+			''' Parsing code is written with if statement, need to refactor, seems to be too stupid '''
+
+			''' Parsing address <td>'''
+			if (field == fields[0]):
+				address = data.get_text('|', strip = True).split('|')
+				line = ""
+				for i in range(len(address)-2):
+					if (i != len(address)-2):
+						line += address[i] + ", "
+					else:
+						line += address[i]
+				data_row.append(line)
+				data_row.append(address[-1])
+				data_row.append(address[-2])
+				#print address.split('|')
+				#print repr(address).decode("unicode-escape")
+
+			if (field == fields[1]):
+				room = data.get_text()
+				#print repr(room).decode("unicode-escape")
+				data_row.append(room)
+
+			'''Room space'''
+			if (field == fields[2]):
+				rooms = data.get_text('|', strip = True)
+
+				'''Kitchen space'''
+				kitchen = re.search(u'кухня:(\W)\d+', rooms)
+				if (kitchen != None):
+					data_row.append(kitchen.group())
+				else:
+					data_row.append(u'NULL')
+
+				'''Dwelling space'''
+				dspace = re.search(u'жилая:(\W)\d+', rooms)
+				if (dspace != None):
+					data_row.append(dspace.group())
+				else:
+					data_row.append(u'NULL')
+
+				'''Full space'''
+				space = re.search(u'общая:(\W)\d+', rooms)
+				if (space != None):
+					data_row.append(space.group())
+				else:
+					data_row.append(u'NULL')
+				
+			'''Price of the appartments'''
+			if (field == fields[3]):
+				price_raw = data.get_text('|', strip = True).split('|')
+				price = price_raw[0]
+				if (price != None):
+					data_row.append(price)
+				else:
+					data_row.append(u'NULL')
+
+			'''# of the floor'''
+			if (field == fields[4]):
+				floor = data.get_text('|', strip = True)
+				data_row.append(floor)
+
+			'''Additional information'''
+			if (field == fields[5]):
+				addinfo = data.get_text('|', strip = True)
+				data_row.append(addinfo)
+
+		
+		'''End of data to parse, appending row to a table'''		
+		#print repr(data_row).decode("unicode-escape")
+		data_table.append(data_row)
+	#print repr(data_table).decode("unicode-escape")
+
+	# returns an array of arrays with data cells
+	return data_table
 
 if __name__ == "__main__":
-	page = int(sys.argv[1])
-	data = parsePage(page)
-	export(data)
+	url = HREF6
+	page_start = int(sys.argv[1])
+	page_end = int(sys.argv[2]) + 1
+	for page_num in range(page_start,page_end):
+		table = getTable(page_num, url)
+		while (table == []):
+			table = getTable(page_num, url)
+			time.sleep(2)
+		data = parseTable(table)
+		export(data, str(page_num) + '_data.txt')
+		sys.stdout.write(" #{} page is parsed".format(page_num))
+		sys.stdout.flush()
+		#time.sleep(2)
+		sys.stdout.write("\r")
+		sys.stdout.flush()
